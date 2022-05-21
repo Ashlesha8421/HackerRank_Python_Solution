9b2fe69a5feb70183a0f4cd0c25358bc6fbf4e00
Ashlesha Pundlikrao Datir <79134243+Ashlesha8421@users.noreply.github.com>
2022-05-21 12:22:13 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+## This is a sample Python script.
+
+# first Program
+# Here is a sample line of code that can be executed in Python:
+
+my_string = "Hello, World!"
+print("1st result:",my_string)
+
+#Second program
+def print_hi(name):
+    print(f'2nd result:, {name}')
+
+if __name__ == '__main__':
+    print_hi('Hello, World!')
+
